a075ef85a67fbcefe72baa8de7167543639f23f0
YanaGushlevskaya <Jasynja@ukr.net>
2019-07-16 18:45:01 +0300

    changed const style to camelcase, deleted 'return' from arrow functions, added one ternary operator

--- FL11_HW9/homework/index.js
+++ FL11_HW9/homework/index.js
@@ -34,52 +34,46 @@ let executeforEach = (arr, func) => {
 
 let mapArray = (arr, func) => {
   let newArr = [];
-  executeforEach(arr, i => {
-    return newArr.push(func(i));
-  });
+  executeforEach(arr, i => newArr.push(func(i)));
   return newArr;
 };
 
 let filterArray = (arr, func) => {
   let newArr = [];
-  executeforEach(arr, i => {
-    if (func(i)) {
-      return newArr.push(i);
-    }
-  });
+  executeforEach(arr, i => func(i) ? newArr.push(i): null);
   return newArr;
 };
 
 let showFormattedDate = obj => {
-  const START = 4;
-  const NEWDATE = obj.toDateString().substring(START);
-  return `Date: ${NEWDATE}`;
+  const start = 4;
+  const newDate = obj.toDateString().substring(start);
+  return `Date: ${newDate}`;
 };
 
 let canConvertToDate = str => !isNaN(Date.parse(str));
 
 let daysBetween = (obj1, obj2) => {
-  const SEC = 1000;
-  const MIN = 60;
-  const HOUR = 60;
-  const DAY = 24;
+  const sec = 1000;
+  const min = 60;
+  const hour = 60;
+  const day = 24;
 
-  const ONEDAY = SEC * MIN * HOUR * DAY;
-  const DATE1 = obj1.getTime();
-  const DATE2 = obj2.getTime();
-  const DIFF = Math.round((DATE2 - DATE1) / ONEDAY);
-  return DIFF;
+  const oneDay = sec * min * hour * day;
+  const date1 = obj1.getTime();
+  const date2 = obj2.getTime();
+  const diff = Math.round((date2 - date1) / oneDay);
+  return diff;
 };
 
 let getAmountOfAdultPeople = data => {
   let dateNow = new Date();
-  const DAYSINYEAR = 365;
-  const ADULTAGE = 18;
-  const ADULT = DAYSINYEAR * ADULTAGE;
+  const daysInYear = 365;
+  const adultAge = 18;
+  const adult = daysInYear * adultAge;
 
   let counter = filterArray(data, i => {
     let formattedDate = new Date(i['birthday']);
-    return daysBetween(formattedDate, dateNow) >= ADULT;
+    return daysBetween(formattedDate, dateNow) >= adult;
   });
 
   return counter.length;
